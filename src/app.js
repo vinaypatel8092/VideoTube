710c97a57a7c154b159ac08d00f9c865b644f34f
Vinay Patel <vinaypatel7198@gmail.com>
2024-06-16 12:24:53 +0530

    util code setup(asyncHandler, ApiError and ApiResponse)

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+// for parsing json body
+app.use(express.json({ limit: "16kb" }));
+// for parsing url encoded body
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// serve static folder/files
+app.use(express.static("public"));
+// accessing cookie and performing CRUD op. on cookiee
+app.use(cookieParser());
+
+export { app };
